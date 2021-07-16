f2f830a36901b67aa5f25d01199c2ff4bd8eb60f
TheHamkerCat <thehamkercat@gmail.com>
2021-07-16 09:43:24 +0530

    url

--- spr/__main__.py
+++ spr/__main__.py
@@ -44,7 +44,7 @@ async def main():
 @spr.on_message(filters.command(["help", "start"]), group=2)
 async def help_command(_, message: Message):
     if message.chat.type != "private":
-        kb = ikb({"Help": f"t.me/{BOT_USERNAME}?start=help"})
+        kb = ikb({"Help": f"https://t.me/{BOT_USERNAME}?start=help"})
         return await message.reply("Pm Me For Help", reply_markup=kb)
     kb = ikb(
         {

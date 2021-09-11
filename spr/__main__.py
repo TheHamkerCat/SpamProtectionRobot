ded889f5a5874b0aa896c05ff12364f3c79d80d5
ultroi <86416271+ultroi@users.noreply.github.com>
2021-09-11 05:36:58 +0530

    Update __main__.py

--- spr/__main__.py
+++ spr/__main__.py
@@ -51,7 +51,7 @@ async def help_command(_, message: Message):
             "Help": "bot_commands",
             "Repo": "https://github.com/TheHamkerCat/SpamProtectionRobot",
             "Add Me To Your Group": f"https://t.me/{BOT_USERNAME}?startgroup=new",
-            "Support Chat (for now)": "https://t.me/WBBSupport",
+
         }
     )
     mention = message.from_user.mention

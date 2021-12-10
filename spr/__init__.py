335b03b45bac41ad2dcdc13d1f9ca5ebca6c6914
Akshay Rajput <thehamkercat@gmail.com>
2021-12-10 18:53:28 +0530

    Changed API url again

--- spr/__init__.py
+++ spr/__init__.py
@@ -9,7 +9,7 @@ SESSION_NAME = "spr"
 DB_NAME = "db.sqlite3"
 API_ID = 6
 API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
-ARQ_API_URL = "https://grambuilders.tech"
+ARQ_API_URL = "https://thearq.tech"
 
 if exists("config.py"):
     from config import *

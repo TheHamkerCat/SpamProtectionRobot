190d661cc7f78a07182d1e35cf53c12f0d3ac304
Vivek <vivektp2006@gmail.com>
2024-01-13 18:39:08 +0530

    Update __main__.py

--- spr/__main__.py
+++ spr/__main__.py
@@ -52,7 +52,7 @@ async def help_command(_, message: Message):
         {
             "Help": "bot_commands",
             "Support": "https://t.me/iyo_obot",
-            "\nAdd Me To Your Group": f"https://t.me/{BOT_USERNAME}?startgroup=new",
+            "Add Me To Your Group": f"https://t.me/{BOT_USERNAME}?startgroup=new",
         }
     )
     mention = message.from_user.mention
